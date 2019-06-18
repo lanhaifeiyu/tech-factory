110136c6f05813c68a2f5ee3d0025ec22d0965f0
yurh <airson_yu@163.com>
2019-06-18 10:39:23 +0800

    init

---
+++ src/main/java/com/lhfeiyu/tech/factory/TechFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.lhfeiyu.tech.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechFactoryApplication.class, args);
+    }
+
+}
